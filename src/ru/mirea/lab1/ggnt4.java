187eb8ccdd6a7c31aaa6807556ab5dfdbaefa33e
pc1 <islamkaev0210@gmail.com>
2023-10-11 00:37:13 +0300

    Merge remote-tracking branch 'origin/main'

---
+++ src/ru/mirea/lab1/ggnt4.java
@@ -0,0 +1,9 @@
+package ru.mirea.lab1;
+
+public class ggnt4 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 11; i++) {
+            System.out.println(1./i);
+        }
+    }
+}
